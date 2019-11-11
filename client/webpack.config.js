38f24daf77326e23badae54be1aa125507fb00fb
DrizzlingCattus <hit0473@gmail.com>
2019-11-11 17:15:35 +0900

    Chore: client 설정
    eslint, prettier관련 설정을 client로 이동
    react, react-dom 설치
    babel, html, css, react를 위한 webpack설정

---
+++ client/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+
+const config = {
+  mode: "development",
+
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve(__dirname + "/dist"),
+    filename: "bundle.js",
+  },
+
+  devtool: "inline-source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+          options: {
+            minimize: true,
+          },
+        },
+      },
+
+      {
+        test: /\.css$/,
+        use: ["css-loader"],
+      },
+    ], // rules
+  }, // module
+
+  devServer: {
+    contentBase: [path.join(__dirname, "dist")],
+    port: 8080,
+    host: "0.0.0.0",
+    open: false,
+  },
+};
+
+module.exports = config;
