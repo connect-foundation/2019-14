f0ac61dd083d3773eb0b5c1c5359e23f311d06b4
dimsssss <caruen@hanmail.net>
2019-11-12 20:16:22 +0900

    Feat: 에디터 헤더 컴포넌트
    에디터 화면에 헤더 컴포넌트 구현
    하위 컴포넌트로 헤더 메인과
    editor tool bar 컴포넌트가 있다

---
+++ client/src/components/EditorHeader.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+// TODO 차후에 CSS 스타일이 고정되면 그에 맞게 수정한다.
+const Header = styled.header`
+  display: flex;
+  flex-direction: row;
+  border: 1px solid green;
+  width: 100%;
+  height: 10%;
+`;
+
+/**
+ * 에디터 헤더 컴포넌트
+ * 헤더 메인(로고, 타이틀, 선택한 환경 아이콘들)과
+ * Editor Tool Bar 컴포넌트를 import한다.
+ */
+
+function EditorHeader() {
+  // TODO boost writer 하드 코딩값 지우기
+  return <Header>boost writer</Header>;
+}
+
+export default EditorHeader;
