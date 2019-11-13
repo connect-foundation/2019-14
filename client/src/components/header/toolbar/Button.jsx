fefc88600778c36260fc050887f65ce049c10caf
RBJH <jhyun9356@gmail.com>
2019-11-13 16:51:46 +0900

    Feat: Button 아이콘 적용
    아이콘 적용 생산성 향상을 위해 fontawesome 모듈 사용
    New Editor와 같은 의미에 적합한 아이콘 적용

--- client/src/components/header/toolbar/Button.jsx
+++ client/src/components/header/toolbar/Button.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileMedical } from "@fortawesome/free-solid-svg-icons";
 
 const Button = () => {
-  return <></>;
+  return (
+    <>
+      <FontAwesomeIcon icon={faFileMedical} />
+    </>
+  );
 };
 
 export default Button;
