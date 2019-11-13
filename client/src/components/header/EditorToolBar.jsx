58cf6daf888494276e4c3c7ea74a1d59e45a145d
RBJH <jhyun9356@gmail.com>
2019-11-13 16:39:19 +0900

    Feat: Editor Tool Bar 컴포넌트 구현
    화면 기획서에 명시된 버튼 6개를 배치하기 위한 스켈레톤 코드 작성

---
+++ client/src/components/header/EditorToolBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const EditorToolBar = () => {
+  return (
+    <>
+      <button />
+      <button />
+      <button />
+      <button />
+      <button />
+      <button />
+    </>
+  );
+};
+
+export default EditorToolBar;
