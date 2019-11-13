b2c55f47209e58ab96498b1ab1388f747fa70c20
RBJH <jhyun9356@gmail.com>
2019-11-13 16:47:31 +0900

    Feat: Button 컴포넌트 구현
    버튼별 속성을 정의하기 위해 버튼 컴포넌트 구현
    ToolBar의 컴포넌트화를 위해 디렉토리 구조 수정
    버튼의 아이콘 삽입의 편의성을 위해 fontawsome 모듈 설치
    CC 저작권 표시 고려 필요

---
+++ client/src/components/header/toolbar/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = () => {
+  return <></>;
+};
+
+export default Button;
---
+++ client/src/components/header/toolbar/ToolBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Button from "./Button";
+
+const EditorToolBar = () => {
+  return (
+    <>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </>
+  );
+};
+
+export default EditorToolBar;
