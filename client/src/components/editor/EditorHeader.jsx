3b1cde1e9166948b6e66f1a65cd95cbaeb87c7e6
dimsssss <caruen@hanmail.net>
2019-11-14 17:17:54 +0900

    Feat: 구현한 wrapper추가
    작성한 wrapper를 로드하는 코드 추가

--- client/src/components/editor/EditorHeader.jsx
+++ client/src/components/editor/EditorHeader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Logo from "./Logo";
-import DocumentTitleInput from "./DocumentTitle";
+import DocumentTitleWrapper from "./DocumentTitleWrapper";
 // TODO 차후에 CSS 스타일이 고정되면 그에 맞게 수정한다.
 const Header = styled.header`
   display: flex;
@@ -22,7 +22,7 @@ const EditorHeader = () => {
   return (
     <Header>
       <Logo />
-      <DocumentTitleInput />
+      <DocumentTitleWrapper />
     </Header>
   );
 };
