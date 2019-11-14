9567b867271d1fb52493d6b9ddfc5be806d7973e
dimsssss <caruen@hanmail.net>
2019-11-14 18:39:30 +0900

    Feat: 구현한 컴포넌트 로드
    터미널 아이콘과 관련된 컴포넌트 로드
    관련된 기본 이미지들 추가

--- client/src/components/editor/EditorHeader.jsx
+++ client/src/components/editor/EditorHeader.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Logo from "./Logo";
 import DocumentTitleWrapper from "./DocumentTitleWrapper";
+import UserTerminalEnvironments from "./UserTerminalEnvironments";
 // TODO 차후에 CSS 스타일이 고정되면 그에 맞게 수정한다.
 const Header = styled.header`
   display: flex;
@@ -23,6 +24,7 @@ const EditorHeader = () => {
     <Header>
       <Logo />
       <DocumentTitleWrapper />
+      <UserTerminalEnvironments />
     </Header>
   );
 };
