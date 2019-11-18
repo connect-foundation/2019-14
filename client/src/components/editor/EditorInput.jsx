bd76ba3a54787fc3ff2318cb1506158a0136e990
RBJH <jhyun9356@gmail.com>
2019-11-18 21:00:24 +0900

    Feat: 마크다운 Heading(h1 ~ h6) 문법 구현
    h1 ~ h6 속성을 분기하기 위해 enum 변수 사용
    코드리뷰 사항을 반영하기 위해 enum 변수명 구체화

--- client/src/components/editor/EditorInput.jsx
+++ client/src/components/editor/EditorInput.jsx
@@ -1,36 +1,94 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
+import MARKDOWN_TYPE from "./MARKDOWN_TYPE";
+
+const MARKDOWN_ATTRIBUTE = {
+  [MARKDOWN_TYPE.DEFAULT]: {
+    placeholder: "",
+    fontSize: "medium",
+    marginTopBottom: "0",
+  },
+  [MARKDOWN_TYPE.H1]: {
+    placeholder: "Heading 1",
+    fontSize: "2em",
+    marginTopBottom: "0.67em",
+  },
+  [MARKDOWN_TYPE.H2]: {
+    placeholder: "Heading 2",
+    fontSize: "1.5em",
+    marginTopBottom: "0.83em",
+  },
+  [MARKDOWN_TYPE.H3]: {
+    placeholder: "Heading 3",
+    fontSize: "1.17em",
+    marginTopBottom: "1em",
+  },
+  [MARKDOWN_TYPE.H4]: {
+    placeholder: "Heading 4",
+    fontSize: "1em",
+    marginTopBottom: "1.33em",
+  },
+  [MARKDOWN_TYPE.H5]: {
+    placeholder: "Heading 5",
+    fontSize: ".83em",
+    marginTopBottom: "1.67em",
+  },
+  [MARKDOWN_TYPE.H6]: {
+    placeholder: "Heading 6",
+    fontSize: ".67em",
+    marginTopBottom: "2.33em",
+  },
+};
+
 const Input = styled.input`
   display: block;
   font-weight: bold;
-  word-wrap: break-word;
   width: 100%;
   border: transparent;
   outline: transparent;
 
-  font-size: ${(props) => (props.type === "h1" ? "2em" : "medium")};
-  margin-top: ${(props) => (props.type === "h1" ? "0.67em" : "0")};
-  margin-bottom: ${(props) => (props.type === "h1" ? "0.67em" : "0")};
+  font-size: ${(props) => MARKDOWN_ATTRIBUTE[props.type].fontSize};
+  margin-top: ${(props) => MARKDOWN_ATTRIBUTE[props.type].marginTopBottom};
+  margin-bottom: ${(props) => MARKDOWN_ATTRIBUTE[props.type].marginTopBottom}};
 `;
 
 const EditorInput = () => {
   const [state, setState] = useState({ value: "", type: "", placeholder: "" });
 
-  const TYPE = {
-    DEFAULT: "",
-    H1: "h1",
-  };
-
   const onChange = (ev) => setState({ ...state, value: ev.target.value });
   const onKeyPress = (ev) => {
     if (ev.key === " ") {
-      if (state.value === "#") setState({ ...state, type: TYPE.H1 });
+      switch (state.value) {
+        case "#":
+          setState({ ...state, type: MARKDOWN_TYPE.H1 });
+          break;
+        case "##":
+          setState({ ...state, type: MARKDOWN_TYPE.H2 });
+          break;
+        case "###":
+          setState({ ...state, type: MARKDOWN_TYPE.H3 });
+          break;
+        case "####":
+          setState({ ...state, type: MARKDOWN_TYPE.H4 });
+          break;
+        case "#####":
+          setState({ ...state, type: MARKDOWN_TYPE.H5 });
+          break;
+        case "######":
+          setState({ ...state, type: MARKDOWN_TYPE.H6 });
+          break;
+      }
     }
   };
 
   useEffect(() => {
-    if (state.type) setState({ ...state, value: "", placeholder: "Heading 1" });
+    if (state.type)
+      setState({
+        ...state,
+        value: "",
+        placeholder: MARKDOWN_ATTRIBUTE[state.type].placeholder,
+      });
   }, [state.type]);
 
   return (
