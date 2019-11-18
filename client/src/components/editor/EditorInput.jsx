2d260f83934104b1740cff2937f3d8924d491f41
RBJH <jhyun9356@gmail.com>
2019-11-18 16:55:56 +0900

    Feat: 마크다운 h1(# ) 파서 구현
    WISIWYG 구현을 위해 onKeyPress 이벤트 사용
    타입 변경 시 값 초기화를 위해 useEffect 사용

--- client/src/components/editor/EditorInput.jsx
+++ client/src/components/editor/EditorInput.jsx
@@ -1,11 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 const EditorInput = () => {
-  const [state, setState] = useState({ value: "" });
+  const [state, setState] = useState({ value: "", type: "" });
+
+  const TYPE = {
+    DEFAULT: "",
+    H1: "h1",
+  };
 
   const onChange = (ev) => setState({ ...state, value: ev.target.value });
+  const onKeyPress = (ev) => {
+    if (ev.key === " ") {
+      if (state.value === "#") setState({ ...state, type: TYPE.H1 });
+    }
+  };
 
-  return <input value={state.value} onChange={onChange} />;
+  useEffect(() => {
+    if (state.type) setState({ ...state, value: "" });
+  }, [state.type]);
+
+  return (
+    <input
+      type={state.type}
+      value={state.value}
+      onChange={onChange}
+      onKeyPress={onKeyPress}
+    />
+  );
 };
 
 export default EditorInput;
