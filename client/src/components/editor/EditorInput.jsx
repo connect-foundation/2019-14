a02c670d77ddd60270188d84c5bff6d829ec7c27
RBJH <jhyun9356@gmail.com>
2019-11-18 20:02:24 +0900

    Feat: EditorInput 디자인
    사용자의 입력을 받는 태그이므로 input 태그 사용
    WYSIWYG 적용을 알려주기 위해 placeholder 표시
    h1 태그의 적용을 위해 styled-components 패키지 사용

--- client/src/components/editor/EditorInput.jsx
+++ client/src/components/editor/EditorInput.jsx
@@ -1,7 +1,21 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const Input = styled.input`
+  display: block;
+  font-weight: bold;
+  word-wrap: break-word;
+  width: 100%;
+  border: transparent;
+  outline: transparent;
+
+  font-size: ${(props) => (props.type === "h1" ? "2em" : "medium")};
+  margin-top: ${(props) => (props.type === "h1" ? "0.67em" : "0")};
+  margin-bottom: ${(props) => (props.type === "h1" ? "0.67em" : "0")};
+`;
 
 const EditorInput = () => {
-  const [state, setState] = useState({ value: "", type: "" });
+  const [state, setState] = useState({ value: "", type: "", placeholder: "" });
 
   const TYPE = {
     DEFAULT: "",
@@ -16,13 +30,14 @@ const EditorInput = () => {
   };
 
   useEffect(() => {
-    if (state.type) setState({ ...state, value: "" });
+    if (state.type) setState({ ...state, value: "", placeholder: "Heading 1" });
   }, [state.type]);
 
   return (
-    <input
+    <Input
       type={state.type}
       value={state.value}
+      placeholder={state.placeholder}
       onChange={onChange}
       onKeyPress={onKeyPress}
     />
