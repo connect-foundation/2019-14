2de5a060d3d9d0644c639ccb9e36ec8d3e36b887
dimsssss <caruen@hanmail.net>
2019-11-18 23:39:21 +0900

    Feat: 코드리뷰 수정사항 반영
    props-type 사용
    key 추가
    
    PR #86

--- client/src/components/editor/TerminalImage.jsx
+++ client/src/components/editor/TerminalImage.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from 'prop-types';
 
 const TerminalImg = styled.img`
   margin-left: 1.5rem;
 `;
 
-const TerminalImage = (imagePath, { isDragabble } = TerminalImage.defaultProps) => {
-  return <TerminalImg alt="" src={imagePath} draggable={isDragabble} />;
+const TerminalImage = (imagePath, index, props = TerminalImage.defaultProps) => {
+  return <TerminalImg src={imagePath} alt={props.terminalEnvironment} draggable={props.isDragabble} key={index} />;
 };
 
+TerminalImage.propTypes = {
+  isDragabble: PropTypes.bool,
+  terminalEnvironment: PropTypes.string,
+}
+
 TerminalImage.defaultProps = {
-  isDragabble: "false"
+  isDragabble: false,
+  terminalEnvironment: "bash",
 }
 
 export default TerminalImage;
--- client/src/components/editor/UserTerminalEnvironments.jsx
+++ client/src/components/editor/UserTerminalEnvironments.jsx
@@ -1,35 +1,43 @@
 import React from "react";
 import styled from "styled-components";
 import TerminalImage from "./TerminalImage";
+import PropTypes from 'prop-types';
 // TODO 임시로 저장한 기본 아이콘 삭제할 것
 import ubuntuImagePath from "../../../public/ubuntu-50.png";
 import mysqlImagePath from "../../../public/mysql-logo-50.png";
 import nodejsImagePath from "../../../public/nodejs-50.png";
 import npmImagePath from "../../../public/npm-50.png";
 
-const UserTerminalEnvironmentContainer = styled.section`
+const UserTerminalEnvironmentImageList = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: center;
 `;
 
-const UserTerminalEnvironments = () => {
-  const defaultData = [
-    ubuntuImagePath,
-    mysqlImagePath,
-    nodejsImagePath,
-    npmImagePath,
-  ];
+const UserTerminalEnvironments = (props) => {
   const makeImageList = () => {
-    return defaultData.map(image => {
-      return TerminalImage(image);
+    return props.defaultImages.map((image, index) => {
+      return TerminalImage(image, index);
     });
   };
   return (
-    <UserTerminalEnvironmentContainer>
+    <UserTerminalEnvironmentImageList>
       {makeImageList()}
-    </UserTerminalEnvironmentContainer>
+    </UserTerminalEnvironmentImageList>
   );
 };
+
+UserTerminalEnvironments.propTypes = {
+  defaultImages: PropTypes.arrayOf(PropTypes.string),
+}
+
+UserTerminalEnvironments.defaultProps = {
+  defaultImages: [ 
+    ubuntuImagePath,
+    mysqlImagePath,
+    nodejsImagePath,
+    npmImagePath
+  ],
+}
 
 export default UserTerminalEnvironments;
