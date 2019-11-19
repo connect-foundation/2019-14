012ad8cbe31f5fff95fa27d3ad1c6c3fa6dffe96
ImuruKevol <kwon3286@korea.ac.kr>
2019-11-19 13:56:05 +0900

    Chore: JSdoc 스타일 수정
    짧은 설명은 두 줄에서 한 줄로 합침

--- client/src/actions/CellAction.js
+++ client/src/actions/CellAction.js
@@ -12,10 +12,8 @@ const CELL_ACTION = {
 const cellActionCreator = {
   /**
    * 셀을 생성한다.
-   * @returns type
-   * - Cell Action Type
-   * @returns renderTarget
-   * - 새로 생성한 Cell
+   * @returns type: 셀 액션 타입
+   * @returns renderTarget: 새로 생성한 셀 컴포넌트
    */
   new(renderTarget) {
     return {
@@ -25,16 +23,11 @@ const cellActionCreator = {
   },
   /**
    * 셀의 데이터를 초기화시킨다.
-   * @param {Number} index
-   * - 초기화할 셀의 index
-   * @param {Text} text
-   * - 초기 데이터
-   * @returns type
-   * - 셀 액션 타입
-   * @returns renderTarget
-   * - 데이터를 [text]로 초기화할 Cell(React Component)
-   * @returns text
-   * - 초기화 데이터
+   * @param {Number} index 초기화할 셀의 index
+   * @param {Text} text 초기 데이터
+   * @returns type: 셀 액션 타입
+   * @returns renderTarget: 데이터를 [text]로 초기화할 셀 컴포넌트
+   * @returns: text 초기화 데이터
    */
   init(index, text) {
     return {
@@ -45,14 +38,10 @@ const cellActionCreator = {
   },
   /**
    * 셀의 텍스트를 변경한다.
-   * @param {Text} text
-   * - 변경된 텍스트
-   * @returns type
-   * - 셀 액션 타입
-   * @returns renderTarget
-   * - 데이터를 [text]로 변경할 Cell
-   * @returns text
-   * - 변경된 텍스트
+   * @param {Text} text 변경된 텍스트
+   * @returns type: 셀 액션 타입
+   * @returns renderTarget: 데이터를 [text]로 변경할 Cell
+   * @returns text: 변경된 텍스트
    */
   input(text) {
     return {
@@ -86,6 +75,7 @@ const cellActionCreator = {
    * - 변경된 Cell(React Component)
    * - @todo 이 부분은 라인 파서와 연동이 필요함. 추가로 데이터 파라미터를 받아야 할 듯.
    * - 라인 파서로 변경된 데이터가 반영된 텍스트를 받아서 store에 넣어줘야 함
+   * - @todo 현재 무슨 태그인지 저장하기
    */
   transform(renderTarget, currentIndex, text) {
     return {
