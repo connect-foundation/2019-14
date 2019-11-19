0d2e8eb693d414c6240358197c19cf152c78b7c4
RBJH <jhyun9356@gmail.com>
2019-11-19 19:17:22 +0900

    Feat: EditorInput 컴포넌트 태그 구현
    스타일 통일성을 위해 styled-componentsDml HTML 태그를 활용

--- client/src/components/editor/EditorInput.jsx
+++ client/src/components/editor/EditorInput.jsx
@@ -1,67 +1,74 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
-const HEADING_ATTRIBUTE = {
-  diplay: "block",
-  fontWeight: "bold",
+const MARKDOWN_TYPE = {
+  "": styled.div`
+    :focus {
+      outline: none;
+    }
+  `,
+
+  h1: styled.h1`
+    :empty:before {
+      content: attr(placeholder);
+      color: gray;
+    }
+
+    :focus {
+      outline: none;
+    }
+  `,
+  h2: styled.h2`
+    :empty:before {
+      content: attr(placeholder);
+      color: gray;
+    }
+
+    :focus {
+      outline: none;
+    }
+  `,
+  h3: styled.h3`
+    :empty:before {
+      content: attr(placeholder);
+      color: gray;
+    }
+
+    :focus {
+      outline: none;
+    }
+  `,
+  h4: styled.h4`
+    :empty:before {
+      content: attr(placeholder);
+      color: gray;
+    }
+
+    :focus {
+      outline: none;
+    }
+  `,
+  h5: styled.h5`
+    :empty:before {
+      content: attr(placeholder);
+      color: gray;
+    }
+
+    :focus {
+      outline: none;
+    }
+  `,
+  h6: styled.h6`
+    :empty:before {
+      content: attr(placeholder);
+      color: gray;
+    }
+
+    :focus {
+      outline: none;
+    }
+  `,
 };
-
-const MARKDOWN_ATTRIBUTE = {
-  "": {
-    placeholder: "",
-    fontSize: "medium",
-    marginTopBottom: "0",
-  },
-
-  H1: {
-    ...HEADING_ATTRIBUTE,
-    placeholder: "Heading 1",
-    fontSize: "2em",
-    marginTopBottom: "0.67em",
-  },
-  H2: {
-    ...HEADING_ATTRIBUTE,
-    placeholder: "Heading 2",
-    fontSize: "1.5em",
-    marginTopBottom: "0.83em",
-  },
-  H3: {
-    ...HEADING_ATTRIBUTE,
-    placeholder: "Heading 3",
-    fontSize: "1.17em",
-    marginTopBottom: "1em",
-  },
-  H4: {
-    ...HEADING_ATTRIBUTE,
-    placeholder: "Heading 4",
-    fontSize: "1em",
-    marginTopBottom: "1.33em",
-  },
-  H5: {
-    ...HEADING_ATTRIBUTE,
-    placeholder: "Heading 5",
-    fontSize: ".83em",
-    marginTopBottom: "1.67em",
-  },
-  H6: {
-    ...HEADING_ATTRIBUTE,
-    placeholder: "Heading 6",
-    fontSize: ".67em",
-    marginTopBottom: "2.33em",
-  },
-};
-
-const Input = styled.input`
-  width: 100%;
-  border: transparent;
-  outline: transparent;
-
-  display: ${(props) => MARKDOWN_ATTRIBUTE[props.type].display};
-  font-weight: ${(props) => MARKDOWN_ATTRIBUTE[props.type].fontWeight};
-  font-size: ${(props) => MARKDOWN_ATTRIBUTE[props.type].fontSize};
-  margin-top: ${(props) => MARKDOWN_ATTRIBUTE[props.type].marginTopBottom};
-  margin-bottom: ${(props) => MARKDOWN_ATTRIBUTE[props.type].marginTopBottom}};
-`;
 
 const EditorInput = () => {
   const [state, setState] = useState({
@@ -71,15 +78,17 @@ const EditorInput = () => {
   });
 
   const STATE = {
-    "#": { ...state, type: "H1" },
-    "##": { ...state, type: "H2" },
-    "###": { ...state, type: "H3" },
-    "####": { ...state, type: "H4" },
-    "#####": { ...state, type: "H5" },
-    "######": { ...state, type: "H6" },
+    "#": { ...state, type: "h1", placeholder: "Heading 1" },
+    "##": { ...state, type: "h2", placeholder: "Heading 2" },
+    "###": { ...state, type: "h3", placeholder: "Heading 3" },
+    "####": { ...state, type: "h4", placeholder: "Heading 4" },
+    "#####": { ...state, type: "h5", placeholder: "Heading 5" },
+    "######": { ...state, type: "h6", placeholder: "Heading 6" },
   };
 
-  const onChange = (ev) => setState({ ...state, value: ev.target.value });
+  const onInput = (ev) => {
+    setState({ ...state, value: ev.target.textContent });
+  };
   const onKeyPress = (ev) => {
     const { key } = ev;
 
@@ -93,16 +102,16 @@ const EditorInput = () => {
       setState({
         ...state,
         value: "",
-        placeholder: MARKDOWN_ATTRIBUTE[state.type].placeholder,
       });
   }, [state.type]);
 
+  const MarkdownWrapper = MARKDOWN_TYPE[state.type];
+
   return (
-    <Input
-      type={state.type}
-      value={state.value}
+    <MarkdownWrapper
       placeholder={state.placeholder}
-      onChange={onChange}
+      contentEditable={true}
+      onInput={onInput}
       onKeyPress={onKeyPress}
     />
   );
