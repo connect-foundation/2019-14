957930cee425dddf7858edc8eb4fdd387bdca3a1
DrizzlingCattus <hit0473@gmail.com>
2019-11-19 15:57:14 +0900

    Feat: 서버에 기본 유틸리티 함수 제공
    어느곳에서나 쓸 수 있는 의존성없는 함수를 제공함
    - string type인지 체크하는 함수
    - function type인지 체크하는 함수
    - express async error를 상위 미들웨어에서 처리하게 넘겨주는 함수

---
+++ server/src/utils/index.js
@@ -0,0 +1,19 @@
+const { StreamResolver } = require("./StreamResolver");
+
+const utils = {};
+
+utils.wrapAsync = (fn) => {
+  return (req, res, next) => {
+    fn(req, res, next).catch(next);
+  };
+};
+
+utils.isString = (v) => {
+  return v instanceof String || typeof v === "string";
+};
+
+utils.isFunction = (v) => {
+  return typeof v === "function";
+};
+
+module.exports = { utils, StreamResolver };
