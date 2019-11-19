a0c0dbfff764701cfa19516c3ecd01be0be6ea6a
ImuruKevol <kwon3286@korea.ac.kr>
2019-11-19 14:55:17 +0900

    Fix: PR 코멘트 반영
    1. 삼항연산자 제거(if문으로 교체)
    2. 반복되는 작업(배열의 지정된 인덱스에 데이터 추가/변경) 추상화
    
    PR #88

--- client/src/reducers/CellReducer.js
+++ client/src/reducers/CellReducer.js
@@ -1,24 +1,50 @@
 import { CELL_ACTION } from "../actions/CellAction";
 
+const arrayInterceptor = {
+  /**
+   * @param {Array} array 데이터를 끼워넣을 배열
+   * @param {Number} cur 현재 인덱스
+   * @param {any} data 끼워넣을 데이터
+   * @returns {Array} 데이터가 추가된 배열
+   */
+  ADD: (array, cur, data) => {
+    if (array.length > 0) {
+      return [
+        ...array.slice(0, cur + 1),
+        data,
+        ...array.slice(cur + 1, array.length),
+      ];
+    }
+    return [data];
+  },
+  /**
+   * @param {Array} array 데이터를 변경할 배열
+   * @param {Number} cur 현재 인덱스
+   * @param {any} data 변경할 데이터
+   * @returns {Array} 데이터가 변경된 배열
+   */
+  CHANGE: (array, cur, data) => {
+    if (array.length > 0) {
+      return [
+        ...array.slice(0, cur),
+        data,
+        ...array.slice(cur + 1, array.length),
+      ];
+    }
+    return [""];
+  },
+};
+
 const cellReducerHandler = {
   [CELL_ACTION.NEW]: (state, action) => {
     const currentIndex = state.currentIndex > 0 ? state.currentIndex : 0;
-    const cells =
-      state.cells.length > 0
-        ? [
-            ...state.cells.slice(0, currentIndex + 1),
-            action.renderTarget,
-            ...state.cells.slice(currentIndex + 1, state.cells.length),
-          ]
-        : [action.renderTarget];
-    const texts =
-      state.texts.length > 0
-        ? [
-            ...state.texts.slice(0, currentIndex + 1),
-            "",
-            ...state.texts.slice(currentIndex + 1, state.texts.length),
-          ]
-        : [""];
+    const cells = arrayInterceptor.ADD(
+      state.cells,
+      currentIndex,
+      action.renderTarget
+    );
+    const texts = arrayInterceptor.ADD(state.texts, currentIndex, "");
+
     return {
       ...state,
       cells,
@@ -26,6 +52,10 @@ const cellReducerHandler = {
     };
   },
 
+  /**
+   * @todo init의 역할 변경 및 확정시키기
+   * - 지정 인덱스의 셀을 맨 처음 디폴트 인풋 태그로 초기화시킨다.
+   */
   [CELL_ACTION.INIT]: (state, action) => {
     const { index } = action;
     const texts =
@@ -44,14 +74,12 @@ const cellReducerHandler = {
 
   [CELL_ACTION.INPUT]: (state, action) => {
     const { currentIndex } = state;
-    const texts =
-      state.texts.length > 0
-        ? [
-            ...state.texts.slice(0, currentIndex),
-            action.text,
-            ...state.texts.slice(currentIndex + 1, state.texts.length),
-          ]
-        : [""];
+    const texts = arrayInterceptor.CHANGE(
+      state.texts,
+      currentIndex,
+      action.text
+    );
+
     return {
       ...state,
       texts,
@@ -74,16 +102,17 @@ const cellReducerHandler = {
 
   [CELL_ACTION.TARGET.TRANSFORM]: (state, action) => {
     const { currentIndex } = action;
-    const cells = [
-      ...state.cells.slice(0, currentIndex),
-      action.renderTarget,
-      ...state.cells.slice(currentIndex + 1, state.cells.length),
-    ];
-    const texts = [
-      ...state.texts.slice(0, currentIndex),
-      action.text,
-      ...state.texts.slice(currentIndex + 1, state.texts.length),
-    ];
+    const cells = arrayInterceptor.CHANGE(
+      state.cells,
+      currentIndex,
+      action.renderTarget
+    );
+    const texts = arrayInterceptor.CHANGE(
+      state.texts,
+      currentIndex,
+      action.text
+    );
+
     return {
       ...state,
       cells,
