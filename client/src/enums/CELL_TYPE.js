8605c519740dc7b05c186972cef1a4c27cbe2cfe
ImuruKevol <kwon3286@korea.ac.kr>
2019-11-23 15:15:42 +0900

    Chore: 셀의 태그에 대한 enumeration을 정의
    [사용자 정의 타입 이름]: [실제 html tag]
    의 형태로 정의

---
+++ client/src/enums/CELL_TYPE.js
@@ -0,0 +1,18 @@
+const CELL_TYPE = {
+  DEFAULT: null,
+  LIST: {
+    UL: "ul",
+    OL: "ol",
+    LI: "li",
+  },
+  HEADING: {
+    H1: "h1",
+    H2: "h2",
+    H3: "h3",
+    H4: "h4",
+    H5: "h5",
+    H6: "h6",
+  },
+};
+
+export default CELL_TYPE;
