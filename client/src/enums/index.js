ae196dcbd49a50f1926748f91caa91019e255dd9
ImuruKevol <kwon3286@korea.ac.kr>
2019-11-25 17:11:07 +0900

    Feat: Event 타입에 대한 Enumeration 생성
    엔터, 탭, 백스페이스 등과 같은 이벤트에 대한 이벤트 타입 생성

---
+++ client/src/enums/index.js
@@ -0,0 +1,6 @@
+import CELL_TAG from "./CELL_TAG";
+import MARKDOWN from "./MARKDOWN";
+import THEME from "./THEME";
+import EVENT_TYPE from "./EVENT_TYPE";
+
+export { CELL_TAG, MARKDOWN, THEME, EVENT_TYPE };
