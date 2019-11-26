61c51294acd82af01589c8d96cc2ffcd9d55be5e
DrizzlingCattus <hit0473@gmail.com>
2019-11-26 18:32:48 +0900

    Feat: Terminal cell store를 제공
    Terminal cell store를 reducer를 붙힌 상태로 제공하고 있다. 현재는 따로
    사용되지않고있다. reducer 테스트가 어느정도 진행이되면 직접 terminal
    cell에 붙혀서 사용해볼생각이다.

---
+++ client/src/stores/TerminalStore.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import propTypes from "prop-types";
+import terminalReducer from "../reducers/TerminalReducer";
+import { useReducerAsync } from "../utils";
+
+const TerminalContext = React.createContext();
+const TerminalDispatchContext = React.createContext();
+
+const makeInitState = () => {
+  return {
+    focusIndex: 0,
+    currentText: "",
+    inputTexts: [],
+    outputTexts: [],
+    replCount: 0,
+  };
+};
+
+const TerminalStore = ({ children }) => {
+  const [terminalState, dispatch] = useReducerAsync(
+    terminalReducer,
+    makeInitState()
+  );
+
+  return (
+    <TerminalContext.Provider value={{ terminalState }}>
+      <TerminalDispatchContext.Provider value={dispatch}>
+        {children}
+      </TerminalDispatchContext.Provider>
+    </TerminalContext.Provider>
+  );
+};
+
+TerminalStore.defaultProps = {
+  children: {},
+};
+
+TerminalStore.propTypes = {
+  children: propTypes.element,
+};
+
+export { TerminalStore, TerminalContext, TerminalDispatchContext };
