46e4f0ac25382e57b42f89af6acf8e2924c17156
ImuruKevol <kwon3286@korea.ac.kr>
2019-11-29 10:31:18 +0900

    리뷰 코드 추출
    - 입력 내용에 따른 세이브/로드 기능에 대한 설계가 필요
    - 인라인 파서, 원라인 파서의 결과물에 따른 결과물 구상

---
+++ client/stores/CellStore.jsx
@@ -0,0 +1,43 @@
+import React, { useReducer } from "react";
+import propTypes from "prop-types";
+import cellReducer from "../reducers/CellReducer";
+import { UuidManager } from "../utils";
+
+const CellContext = React.createContext();
+const CellDispatchContext = React.createContext();
+
+const CellStore = ({ children }) => {
+  const [state, dispatch] = useReducer(cellReducer, {
+    currentIndex: 0,
+    inputRef: null,
+    uuidManager: new UuidManager(),
+    cells: [],
+    texts: [],
+    tags: [],
+    cursor: {
+      start: 0,
+      end: 0,
+    },
+    start: null,
+  });
+
+  return (
+    <CellContext.Provider value={{ state }}>
+      <CellDispatchContext.Provider value={dispatch}>
+        {children}
+      </CellDispatchContext.Provider>
+    </CellContext.Provider>
+  );
+};
+
+CellStore.defaultProps = {
+  // children: [],
+  children: {},
+};
+
+CellStore.propTypes = {
+  children: propTypes.element,
+  // children: propTypes.arrayOf(propTypes.element),
+};
+
+export { CellContext, CellDispatchContext, CellStore };
