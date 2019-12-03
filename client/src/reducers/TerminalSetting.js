d060f688f68d14967971002b852fecdea44596f9
RBJH <jhyun9356@gmail.com>
2019-12-03 17:02:45 +0900

    Refactor: reduce를 객체구조로 변경
    dispatch에 해당하는 함수를 객체에 넣음
    reducer와 handler로 분리

--- client/src/reducers/TerminalSetting.js
+++ client/src/reducers/TerminalSetting.js
@@ -1,9 +1,25 @@
 import { TERMINAL_SETTING_ACTION } from "./../actions/TerminalSetting";
 
-const terminalSettingReducer = (state, action) => {
-  if (action.type === TERMINAL_SETTING_ACTION.SELECT.OS) {
+const terminalSettingHendler = {
+  [TERMINAL_SETTING_ACTION.SELECT.OS]: (state, action) => {
     return { ...state, os: action.os };
+  },
+  [TERMINAL_SETTING_ACTION.SELECT.PL]: (state, action) => {
+    return state;
+  },
+  [TERMINAL_SETTING_ACTION.SELECT.DB]: (state, action) => {
+    return state;
+  },
+};
+
+const terminalSettingReducer = (state, action) => {
+  if (!state) {
+    return null;
   }
+
+  const handler = terminalSettingHendler[action.type];
+
+  return handler(state, action);
 };
 
 export default terminalSettingReducer;
