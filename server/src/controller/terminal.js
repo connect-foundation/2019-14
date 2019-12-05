c07a790adabf62679ca42d0f0dfbdb239c9f7b4f
dimsssss <caruen@hanmail.net>
2019-12-05 21:56:49 +0900

    Feat: 이미지 빌드 로직 컨트롤러에 추가
    도커파일을 생성하고 도커 api를 호출하는 코드 추가

---
+++ server/src/controller/terminal.js
@@ -0,0 +1,32 @@
+const { writeDockerfile } = require("../api/makeDockerfile");
+
+const createDefaultTerminal = async (
+  dockerInstance,
+  baseImageName = ["ubuntu"]
+) => {
+  const dockerFilePath = `${process.env.INIT_CWD}/dockerfiles/`;
+  // TODO 사용자 도커 환경 정보 파싱(도커파일 규격에 맞는 형식으로)
+  const dockerfile = baseImageName.reduce((accumulate, element) => {
+    return `${accumulate} FROM ${element}\n`;
+  }, "");
+  await writeDockerfile(dockerfile);
+
+  const containerId = await dockerInstance.createCustomTerminal(dockerFilePath);
+  return containerId;
+};
+
+const startTerminal = async (dockerInstance, containerId) => {
+  const result = await dockerInstance.startContainer(containerId);
+  return result;
+};
+
+const stopTerminal = async (dockerInstance, containerId) => {
+  const result = await dockerInstance.stopContainer(containerId);
+  return result;
+};
+
+module.exports = {
+  createDefaultTerminal,
+  startTerminal,
+  stopTerminal,
+};
