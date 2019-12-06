da07d70e99a564bed93711bbc98294baedecb459
RBJH <jhyun9356@gmail.com>
2019-12-06 11:14:01 +0900

    Fix: 트래비스 린트 에러 해결
    트래비스 린트 에러 해결을 위해 상대경로 입력 부분 수정 및
    리듀서에서 액션 타입에 따라 리턴을 하지 않는 경우 리턴 값 생성

--- client/src/reducers/TerminalSetting.js
+++ client/src/reducers/TerminalSetting.js
@@ -1,9 +1,11 @@
-import { TERMINAL_SETTING_ACTION } from "./../actions/TerminalSetting";
+import { TERMINAL_SETTING_ACTION } from "../actions/TerminalSetting";
 
 const terminalSettingReducer = (state, action) => {
   if (action.type === TERMINAL_SETTING_ACTION.SELECT.OS) {
     return { ...state, os: action.os };
   }
+
+  return { state };
 };
 
 export default terminalSettingReducer;
