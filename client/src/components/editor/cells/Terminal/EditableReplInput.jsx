1713c52ca284d18eec01df78e706c9a09948edd0
DrizzlingCattus <hit0473@gmail.com>
2019-12-19 15:50:54 +0900

    Fix: 터미널 셀에서 여러줄을 입력할때 표시가 되지않는 버그 수정
    textarea에서 여러줄을 입력할때 rows 속성을 증가시켜서 여러줄로
    표시되도록 변경

--- client/src/components/editor/cells/Terminal/EditableReplInput.jsx
+++ client/src/components/editor/cells/Terminal/EditableReplInput.jsx
@@ -1,9 +1,16 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const EditableReplInput = styled.textarea`
+const EditorTextarea = styled.textarea`
   width: 100%;
-  margin-left: 20px;
 
+  margin-left: 20px;
+  padding: 0;
+
+  line-height: ${(props) => props.lineHeight}px;
+
+  box-sizing: border-box;
   border: none;
   outline: none;
   box-shadow: none;
@@ -15,5 +22,50 @@ const EditableReplInput = styled.textarea`
   color: inherit;
   font-size: inherit;
 `;
+
+const initState = {
+  value: "",
+  rows: 1,
+  minRows: 1,
+  lineHeight: 24,
+};
+
+const EditableReplInput = React.forwardRef(
+  ({ changeHandler, clickHandler }, ref) => {
+    const [state, setState] = useState({ ...initState });
+
+    const changeHandlerWrapper = (e) => {
+      e.persist();
+      changeHandler(e);
+      const { lineHeight } = state;
+      const { scrollHeight } = e.target;
+      const currentRows = Math.round(scrollHeight / lineHeight);
+      setState((prevState) => {
+        return {
+          ...prevState,
+          rows: currentRows,
+          value: e.target.value,
+        };
+      });
+    };
+
+    return (
+      <EditorTextarea
+        ref={ref}
+        onChange={changeHandlerWrapper}
+        onClick={clickHandler}
+        spellCheck={false}
+        rows={state.rows}
+        line-height={state.lineHeight}
+        value={state.value}
+      />
+    );
+  }
+);
+
+EditableReplInput.propTypes = {
+  changeHandler: PropTypes.func.isRequired,
+  clickHandler: PropTypes.func.isRequired,
+};
 
 export default EditableReplInput;
